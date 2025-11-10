b0f8dcfbee6472e3dc1db6c7440e4a82761caf27
LidorHefets <khsurj3018@gmail.com>
2025-11-10 22:15:01 +0200

    Update .gitignore and clean VS temp files

--- server/Movies/Properties/BL/Movies.cs
+++ server/Movies/Properties/BL/Movies.cs
@@ -1,6 +1,49 @@
 ﻿namespace Movies.Properties.BL
 {
-    public class Movies
+    public class Movie
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Rating { get; set; }
+        public double Income { get; set; }
+        public int Duration { get; set; }
+        public string Language { get; set; }
+
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public string PhotoURL { get; set; }
+
+
+        public static List<Movie> movies = new List<Movie>();
+
+        public bool Insert(Movie m)
+        {
+            try
+            {
+                foreach (Movie movie in movies)
+                {
+                    if (m.Id == Id)
+                    {
+                        return false; // Movie with the same Id already exists
+
+                    }
+                }
+                movies.Add(this);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+
+
+            }
+        }
+        public static List<Movie> Read()
+        {
+            return movies;
+        }
     }
 }
